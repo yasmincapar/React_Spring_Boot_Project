8b80fe9f7042f55ffff0250c5ae3d8404c32f282
yasmincapar <y.capar@yahoo.com>
2024-07-13 13:39:20 +0100

    initial commit

---
+++ src/main/java/com/springbootreactproject/springbootreactproject/SpringbootreactprojectApplication.java
@@ -0,0 +1,13 @@
+package com.springbootreactproject.springbootreactproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootreactprojectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootreactprojectApplication.class, args);
+	}
+
+}
